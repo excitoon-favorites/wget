a9c0a74ed02361897547e73f9e2e9124d776b131
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2018-12-23 23:26:53 +0300

    Moved to BoringSSL.

--- src/http-ntlm.c
+++ src/http-ntlm.c
@@ -177,14 +177,14 @@ setup_des_key(unsigned char *key_56,
 {
   DES_cblock key;
 
-  key[0] = key_56[0];
-  key[1] = ((key_56[0] << 7) & 0xFF) | (key_56[1] >> 1);
-  key[2] = ((key_56[1] << 6) & 0xFF) | (key_56[2] >> 2);
-  key[3] = ((key_56[2] << 5) & 0xFF) | (key_56[3] >> 3);
-  key[4] = ((key_56[3] << 4) & 0xFF) | (key_56[4] >> 4);
-  key[5] = ((key_56[4] << 3) & 0xFF) | (key_56[5] >> 5);
-  key[6] = ((key_56[5] << 2) & 0xFF) | (key_56[6] >> 6);
-  key[7] =  (key_56[6] << 1) & 0xFF;
+  key.bytes[0] = key_56[0];
+  key.bytes[1] = ((key_56[0] << 7) & 0xFF) | (key_56[1] >> 1);
+  key.bytes[2] = ((key_56[1] << 6) & 0xFF) | (key_56[2] >> 2);
+  key.bytes[3] = ((key_56[2] << 5) & 0xFF) | (key_56[3] >> 3);
+  key.bytes[4] = ((key_56[3] << 4) & 0xFF) | (key_56[4] >> 4);
+  key.bytes[5] = ((key_56[4] << 3) & 0xFF) | (key_56[5] >> 5);
+  key.bytes[6] = ((key_56[5] << 2) & 0xFF) | (key_56[6] >> 6);
+  key.bytes[7] =  (key_56[6] << 1) & 0xFF;
 
   DES_set_odd_parity(&key);
   DES_set_key(&key, ks);
